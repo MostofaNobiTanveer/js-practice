e2e605d43f2509918ac14eb7b3b4708f065991c7
unknown <mntanveer9196@gmail.com>
2021-09-05 16:01:49 +0600

    exercise2 solution

--- practice_Day02.js
+++ practice_Day02.js
@@ -1,7 +1,10 @@
 //! ///////////////////////////////////////////
 //! ///////////////////////////////////////////
 //! Array Methods Exercise 01
-
+// {
+//   Development: 2,
+//   Marketing: 1
+// }
 const people = [
   {
     firstName: "Sam",
@@ -91,12 +94,94 @@ const people = [
 
 // Exercises
 
+// Array.prototype.reduce()
 // 1) What is the average income of all the people in the array?
+const salaryAvg =
+  people.reduce((acc, person) => acc + parseInt(person.salary), 0) /
+  people.length;
+
+console.log(salaryAvg.toFixed());
+
+// console.log(
+//   [1, 2, 3].reduce((accumulator, current) => accumulator + current, 0)
+// );
+
+// 0
+// 1 1
+// 2 3
+// 3 6
+// 4 10
+// 5 15
 
 // 2) Who are the people that are currently older than 30?
 
+console.log(new Date("07-07-1978").getFullYear());
+
+const olderThan30 = people
+  .filter(
+    ({ DOB }) => new Date().getFullYear() - new Date(DOB).getFullYear() > 30
+  )
+  .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+
+console.log(String(olderThan30));
+
+// console.log([1,2,3,[4,5,[6,7,[8,9]]]].flat(Infinity))
+
 // 3) Get a list of the people's full name (firstName and lastName).
 
+const fullName = people.map((person) => ({
+  ...person,
+  fullName: `${person.firstName} ${person.lastName}`,
+}));
+console.log(fullName);
+
 // 4) Get a list of people in the array ordered from youngest to oldest.
+// console.log([1, 5, 2, 3, 8].sort((a, b) => a > b ? 1 : -1));
+
+const peopleSorted = people.sort(
+  (personA, personB) =>
+    new Date(personA.DOB).getFullYear() - new Date(personB.DOB).getFullYear()
+);
+console.log(peopleSorted);
 
 // 5) How many people are there in each department?
+
+// {
+//   development: 3,
+//   marketing: 2,
+//   sales: 5,
+// }
+
+const peoplePerDep = people.reduce(
+  (acc, person) => ({
+    ...acc,
+    [person.department]: acc[person.department] + 1 || 1,
+  }),
+  {}
+);
+
+const peoplePerDep = people.reduce((obj, person) => {
+  if (!obj[person.department]) {
+    obj[person.department] = 0;
+  }
+  obj[person.department]++;
+  return obj;
+}, {});
+
+console.log(peoplePerDep);
+
+const mySelf = {
+  name: "Barak Obama",
+  gender: "male",
+};
+// mySelf.age = 45;
+mySelf["age"] = 100;
+
+mySelf["age"]++;
+mySelf["age"]--;
+// mySelf."age/"
+
+console.log(mySelf.name);
+console.log(mySelf["name"]);
+
+console.log(mySelf);
